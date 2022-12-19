e402e2fc82dd63f3f86b181279a3644bbcd06b71
ODAI AL-HEMYARI <121013621+ODAI0@users.noreply.github.com>
2022-12-19 11:57:59 +0300

    Table of multiplication in C++
    A simple code that shows the multiplication table of the numbers from 1-10
    It was done in C++ language by ODAI AL-HEMYARI.

---
+++ Table of multiplication.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{int a,b,c,d,e;
+int i,j;
+
+for(i=1;i<=10;i++)
+{cout<<"The multiplication tabel of "<<i<<endl;
+for(j=1;j<=10;j++)
+cout<<i<<"*"<<j<<'='<<i*j<<endl; 
+	
+	
+}
+	
+	system("pause");
+}
